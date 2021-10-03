735d05f0237b654439369d82442c7d347310a6f6
Pavel Kuzmenko <pavelkuzmenko@gmail.com>
2021-10-03 13:11:24 +0300

    support for resource bundle providers (java 9 and later) for external bundles on modules; targeted for java 11

--- src/main/java/com/networknt/schema/I18nSupport.java
+++ src/main/java/com/networknt/schema/I18nSupport.java
@@ -8,10 +8,10 @@ import java.util.ResourceBundle;
  * @author leaves chen leaves615@gmail.com
  */
 public class I18nSupport {
-    private static final String BASE_NAME = "jsv-messages";
-    private static ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME);
+    private static final String BASE_NAME = "com.networknt.schema.JsvMessages";
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BASE_NAME);
 
     public static String getString(String key) {
-        return bundle.getString(key);
+        return BUNDLE.getString(key);
     }
 }
---
+++ src/main/java/com/networknt/schema/spi/JsvMessagesProvider.java
@@ -0,0 +1,6 @@
+package com.networknt.schema.spi;
+
+import java.util.spi.ResourceBundleProvider;
+
+public interface JsvMessagesProvider extends ResourceBundleProvider {
+}
